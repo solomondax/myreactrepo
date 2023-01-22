e441eb1689f152ba2a490604a87e22dcb70c47a6
solomonp <solodax46@gamil.com>
2023-01-22 16:21:11 +0530

    aapp

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload daxapp.
+          Edit <code>src/App.js</code> and save to reload daxappbb.
         </p>
         <a
           className="App-link"
